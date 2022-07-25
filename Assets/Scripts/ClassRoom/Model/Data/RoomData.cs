2070617a9c7e20d7a347284442aa173fa24ccbc9
Mohsan Saleem <mohsansaleem@gmail.com>
2022-07-25 12:24:57 +0400

    Adding photon

---
+++ Assets/Scripts/ClassRoom/Model/Data/RoomData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PG.ClassRoom.Model.Data
+{
+    public enum EModuleType
+    {
+        Workshop = 0,
+        Decoration
+    }
+
+    public enum ECurrencyType
+    {
+        None = 0,
+        Gold,
+        Happiness
+    }
+    
+    [Serializable]
+    public class ModuleData: GridEntityData
+    {
+        public uint StaticId;
+        public string ModuleName;
+        public EModuleType ModuleType;
+        public ECurrencyType ProductionType;
+        public long Production;
+        public long StorageCapacity;
+        public long CostGold;
+        public long PlayerXP;
+        public int LevelRequired;
+        public string SpriteName;
+
+        public string GetCostString()
+        {
+            return (CostGold > 0 ? (" Gold :" + CostGold) : "");
+        }
+    }
+}
